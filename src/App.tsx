d265a08f3d9335d445d8ea3a6d2ca029254a12d1
muffafa <mm.savar@gmail.com>
2023-03-11 22:47:05 +0300

    redux added

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './pages/Home';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider} from 'react-redux';
 
 function App(): JSX.Element {
   const Stack = createNativeStackNavigator();
 
+  const store = {
+    counter: 0,
+    increment: () => {
+      store.counter += 1;
+    },
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
